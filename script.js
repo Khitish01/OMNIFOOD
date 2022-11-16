0119ababb82c52b0e0bf470033351ce72bc6fd4e
Your Name <you@example.com>
2022-11-16 12:04:03 +0530

    added first project

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".navigation");
+
+btnNav.addEventListener("click", function () {
+    header.classList.toggle("nav-open");
+})
